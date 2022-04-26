cfa8b3a3cfc12a8c8ca31d08817ad6fd5de4a3de
Rickyyy-zh <zhangq_07@outlook.com>
2022-04-26 20:57:12 +0800

    1

---
+++ NN_classfication.py
@@ -0,0 +1,108 @@
+#import sklearn
+import xlrd
+import numpy as np
+import sys
+import os
+import matplotlib.pyplot as plt
+import json
+import torch.nn as nn
+import torch
+from utils import loaddata, data_analysis, plot_curve
+
+class NNmodel(nn.Module):
+    def __init__(self, depth, dims, dropoutP):
+        super().__init__()
+        self.layer = []
+        self.depth = depth
+        self.dropout = dropoutP
+        self.input = nn.Sequential(nn.Linear(16,dims[0]),
+                            nn.ReLU(),
+                            nn.Dropout(self.dropout))
+        for l in range(depth):
+            if l<depth-1:
+                self.layer.append( self.make_layer(dims[l],dims[l+1]))
+        self.middle = nn.Sequential(*self.layer)
+        self.output = nn.Sequential(nn.Linear(dims[-1],7),
+                                nn.Softmax(dim=1))
+
+    def make_layer(self,input_ch, output_ch):
+        l = nn.Sequential(nn.Linear(input_ch,output_ch),
+                        nn.ReLU(),
+                        nn.Dropout(self.dropout))
+        return l
+
+    def forward(self,x):
+        output = self.input(x)
+        # for l in range(self.depth-1):
+        #     output = self.layer[l](output)
+        output = self.middle(output)
+        output = self.output(output)
+        return output
+    
+if __name__ == "__main__":
+    data_dict = loaddata("./DryBeanDataset/Dry_Bean_Dataset.xls")
+    train_data, train_lb, test_data, test_lb, mean, std = data_analysis(data_dict)
+    # print(mean)
+    # print(std)
+
+    train_data = (train_data - mean)/std
+    test_data = (test_data - mean)/std
+    # label = np.eye(label.shape[0], 7, k=0, dtype=np.float32)
+    # device = torch.device("cuda:2") #if torch.cuda.is_available() else "cpu")
+    device = torch.device("cpu") #if torch.cuda.is_available() else "cpu")
+    
+    loss_fn = nn.CrossEntropyLoss()
+
+    model = NNmodel(7,[32,64,128,256,128,64,32],0.1)
+    model.to(device)
+    optimizer = torch.optim.SGD(model.parameters(), lr = 0.5, momentum=0.7, weight_decay=0.0005)
+    step_lr = torch.optim.lr_scheduler.MultiStepLR(optimizer=optimizer,milestones=[5,10,20,40,80], gamma=0.5)
+
+    epochs = 100
+    ax_epoch = list(range(0,epochs))
+    ax_trainloss = []
+    ax_testloss = []
+    ax_acc = []
+    for epoch in range(epochs):
+        model.train()
+        train_loss = 0 
+        test_loss = 0
+        train_num = train_data.shape[0]
+        test_num = test_data.shape[0]
+        for i in range(train_num):
+            input, lb = torch.tensor(train_data[i,:]).to(device), torch.tensor(train_lb[i,:],dtype=torch.long).to(device)
+            input = torch.unsqueeze(input,0)
+            optimizer.zero_grad()
+            out = model(input).to(device)
+            loss = loss_fn(out, lb)
+            loss.backward()
+            optimizer.step()
+            train_loss += loss.item()
+        
+        model.eval()
+        acc =0.0
+        best_acc = 0.0
+        with torch.no_grad():
+            for i in range(test_num):
+                test_in, _lb = torch.tensor(test_data[i,:]).to(device), torch.tensor(test_lb[i,:],dtype=torch.long).to(device)
+                test_in = torch.unsqueeze(test_in,0)
+                out = model(test_in)
+                result = torch.argmax(out)
+                loss = loss_fn(out, _lb)
+                acc += torch.eq(result,_lb).sum().item()
+                test_loss += loss.item()
+
+        print("train epoch [{}/{}]  train_loss:{:.3f} test_loss:{:.3f} accurancy:{:.3f}".format(
+                                    epoch+1, epochs, train_loss/train_num,test_loss/test_num, acc/test_num))
+        step_lr.step()
+        if acc >= best_acc:
+            best_acc = acc
+            torch.save(model, "./best_pt.pt")
+        torch.save(model,"./last_pt.pt")
+
+        ax_trainloss.append( train_loss/train_num)
+        ax_testloss.append(test_loss/test_num)
+        ax_acc.append(acc/test_num)
+    print("finshed training")
+    plot_curve(ax_epoch,ax_trainloss,ax_testloss,ax_acc)
+
---
+++ utils.py
@@ -0,0 +1,95 @@
+# from nbformat import write
+import xlrd
+import numpy as np
+import sys
+import os
+import matplotlib.pyplot as plt
+import json
+import random
+
+def loaddata(path):
+    workbook = xlrd.open_workbook(path)
+    sheet = workbook.sheet_by_index(0)
+    data_num = sheet.nrows -1 
+    feature_num = sheet.ncols -1
+    print("data instance number %d, feature number %d" %(data_num, feature_num))
+    dataset = []
+    feature_name = []
+    label_dict = {"SEKER":0, "BARBUNYA":1, "BOMBAY":2, "CALI":3, "HOROZ":4, "SIRA":5, "DERMASON":6}
+    for row in range(data_num+1):
+        if row ==0:
+            feature_name = sheet.row_values(0)
+        else:
+            dic_sample = {}
+            dic_feature = {}
+            for col in range(feature_num):
+                dic_feature[feature_name[col]] = sheet.cell(row,col).value
+                dic_sample["data"] = dic_feature
+                dic_sample["label"] = label_dict[sheet.cell(row,feature_num).value]
+            dataset.append(dic_sample)
+    
+    # with open(path.replace("xls","json"), "w+") as outputFile:
+    #     sys.stdout = outputFile
+    #     print(json.dumps(dataset))
+    
+    return dataset
+
+def random_number(data_size):
+    number_set = []
+    for i in range(data_size):
+        number_set.append(i)
+    random.seed(9271)
+    random.shuffle(number_set)
+    with open("./randomnum.txt","w") as f:
+        for num in number_set:
+            s = str(number_set[num])+" "
+            f.write(s)
+    return number_set
+
+def data_split(dataset,label,ratio):
+    random_index = random_number(dataset.shape[0])
+    traindata_sz = int( dataset.shape[0]*ratio)
+    train_data = dataset[random_index[:traindata_sz],:]
+    test_data = dataset[random_index[traindata_sz:],:]
+    train_lb = label[random_index[:traindata_sz],:]
+    test_lb = label[random_index[traindata_sz:],:]
+
+    return train_data, train_lb, test_data, test_lb
+
+
+def data_analysis(dic_data):
+    data_num = len(dic_data)
+    label = np.zeros((data_num,1))
+    data = np.zeros((data_num, 16),dtype=np.float32)
+    for i,item in enumerate(dic_data):
+        label[i,0] = item["label"]
+        data_list = []
+        for j, feature in enumerate(item["data"]):
+            data_list.append(item["data"][feature])
+        data[i,:] = np.array((data_list))
+    mean = np.mean(data,axis=0,dtype=np.float32)
+    std  = np.std(data, axis=0,dtype=np.float32)
+
+    train_data, train_label, test_data,test_label = data_split(data, label, ratio=0.9)
+    
+
+    return train_data, train_label, test_data,test_label, mean, std
+
+def plot_curve(idx, data1,data2,data3):
+    fig = plt.figure(figsize=(15,5))
+    
+    ax_train_loss = fig.add_subplot(1,3,1)
+    ax_train_loss.set_title("train loss")
+    ax_train_loss.plot(idx,data1)
+    
+    ax_acc = fig.add_subplot(1,3,3)
+    ax_acc.set_title("test accurancy")
+    ax_acc.plot(idx,data3)
+    
+    ax_val_loss = fig.add_subplot(1,3,2)
+    ax_val_loss.set_title("test loss")
+    ax_val_loss.plot(idx,data2)
+    
+    plt.subplots_adjust(wspace=0.5)
+    
+    plt.savefig("./train_process.jpg")
